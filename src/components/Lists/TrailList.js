44618b93bcd945e6806a6468b639ca4bf99c3e04
FanFan <eric93504@gmail.com>
2021-01-28 20:53:30 +0800

    微調外層grid寬度＆刪除測試資料

--- src/components/Lists/TrailList.js
+++ src/components/Lists/TrailList.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  Grid,
-  GridListTile,
-  GridListTileBar,
-  ListSubheader,
-  IconButton,
-  GridList,
-  Divider,
-} from "@material-ui/core";
-
-import InfoIcon from "@material-ui/icons/Info";
+import { Grid, GridList, Divider } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
   thumb: {
     width: 104,
     height: 72,
@@ -36,69 +29,48 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(2, 0),
   },
 }));
-const image =
-  "https://scontent.frmq2-2.fna.fbcdn.net/v/t1.15752-9/141023215_852309418901747_4316212907466953615_n.jpg?_nc_cat=107&ccb=2&_nc_sid=ae9488&_nc_ohc=fcIbEFbFL64AX_A_9fU&_nc_ht=scontent.frmq2-2.fna&oh=b2c6910b833e54677da0607b416f38bb&oe=6035606F";
-const tileData = [
-  {
-    img: image,
-    title: "巴陵古道",
-    location: "桃園市復興區",
-    distance: 1500,
-  },
-  {
-    img: image,
-    title: "巴陵古道",
-    location: "桃園市復興區",
-    distance: 1500,
-  },
-  {
-    img: image,
-    title: "巴陵古道",
-    location: "桃園市復興區",
-    distance: 1500,
-  },
-  {
-    img: image,
-    title: "巴陵古道",
-    location: "桃園市復興區",
-    distance: 1500,
-  },
-];
 
-export default function TrailList() {
+const TrailList = (props) => {
   const classes = useStyles();
-
+  const data = [props];
   return (
     <div className={classes.root}>
       <GridList cellHeight={72} cols={1}>
-        {tileData.map((tile) => (
+        {data.map((trail) => (
           <Grid
+            key={1}
             container
             direction="row"
             justify="flex-start"
             alignItems="flex-start"
             className={classes.gridCell}
+            item
+            xs={12}
           >
-            <Grid item xs="4">
-              <img src={tile.img} alt={tile.title} className={classes.thumb} />
+            <Grid item xs={4}>
+              <img
+                src={trail.data.coverImage}
+                alt={trail.data.title}
+                className={classes.thumb}
+              />
             </Grid>
             <Grid
               item
-              xs="8"
+              xs={8}
               container
               direction="column"
               alignItems="flex-start"
             >
-              <Grid item xs="12" className={classes.title}>
-                {tile.title}
+              <Grid item xs={12} className={classes.title}>
+                {trail.data.title}
               </Grid>
-              <Grid item xs="12" className={classes.location}>
-                {tile.location}
+              <Grid item xs={12} className={classes.location}>
+                {trail.data.location}
               </Grid>
-              <Grid item xs="12" className={classes.distance}>
-                全程約{tile.distance}公里
+              <Grid item xs={12} className={classes.distance}>
+                全程約{trail.data.distance}公里
               </Grid>
-              <Grid item xs="12">
+              <Grid item xs={12}>
                 <Divider></Divider>
               </Grid>
             </Grid>
@@ -107,4 +79,5 @@ export default function TrailList() {
       </GridList>
     </div>
   );
-}
+};
+export default TrailList;
