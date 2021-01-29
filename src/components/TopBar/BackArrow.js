488fe5fb832adb7b857d1de2727a5d9f27ec5cca
FanFan <eric93504@gmail.com>
2021-01-29 21:03:32 +0800

    微調返回鍵

--- src/components/TopBar/BackArrow.js
+++ src/components/TopBar/BackArrow.js
@@ -3,20 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const useStyles = makeStyles((theme) => ({
-  back_arrowDiv: {
-    width: "100vw",
-  },
-  back_arrow: {
-    /* margin-bottom: 16px; */
-  },
 }));
 
 function BackArrow() {
   const classes = useStyles();
   return (
-    <div className={classes.back_arrowDiv}>
-      <ArrowBackIcon className={classes.back_arrow} />
-    </div>
+      <ArrowBackIcon />
   );
 }
 export default BackArrow;
