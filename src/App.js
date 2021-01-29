1227065aa0a44cf3773411bafc6c7c68d221a763
Dennis <b976543257@gmail.com>
2021-01-29 23:47:12 +0800

    router update Redirect * to searchPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import SearchResult from "./pages/SearchResult";
 import SearchPage from "./pages/SearchPage";
 function App() {
@@ -8,9 +13,9 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/" exact component />
-          <Route path="/searchPage" component={SearchPage} />
-          <Route path="/searchResult" component={SearchResult} />
+          <Route path="/searchPage" exact component={SearchPage} />
+          <Route path="/searchResult" exact component={SearchResult} />
+          <Redirect from="*" to="/searchPage" />
         </Switch>
       </div>
     </Router>
