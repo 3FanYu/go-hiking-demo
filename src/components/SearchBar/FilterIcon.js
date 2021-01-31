c673c9f8b874ced0344b8148ec10d581bcd079fd
FanFan <eric93504@gmail.com>
2021-01-31 13:38:11 +0800

    刪除filter icon component

--- src/components/SearchBar/FilterIcon.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import TuneIcon from "@material-ui/icons/Tune";
-
-function FilterIcon() {
-  return <TuneIcon style={{ color: "#00d04c" }}></TuneIcon>;
-}
-export default FilterIcon;
