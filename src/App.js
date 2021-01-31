5b6c26c4c1f8f56469338f6b286410ffe1305bb9
FanFan <eric93504@gmail.com>
2021-01-31 13:31:44 +0800

    設定預設路由到SearchPage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchResult from "./pages/SearchResult";
 import SearchPage from "./pages/SearchPage";
+
 function App() {
   return (
+    
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/" exact component />
+          <Route path="/" exact component={SearchPage} />
           <Route path="/searchPage" component={SearchPage} />
           <Route path="/searchResult" component={SearchResult} />
         </Switch>
